2051af859dc99829cda72a2ab4a47878ef87c3b7
PabloHNieto <pablo.h.nieto@gmail.com>
2021-04-29 17:12:05 +0200

    Inferring thetas directly: inferTheta function and updating do_DBN

--- code_from_what_genotype_next/dbn-process.R
+++ code_from_what_genotype_next/dbn-process.R
@@ -2,15 +2,13 @@ library(OncoBN)
 
 
 do_DBN <- function(data){
-  out <- fitCPN(data, algorithm="GA") 
-  ## This version of fitCPN is modified in my repo to return theta
-  ## TODO: modify to also use DP
-  
+  out <- fitCPN(data, algorithm="DP") 
   out$edgelist <- replace(out$edgelist, out$edgelist=="WT", "Root")
   parents <- out$edgelist[seq(1, length(out$edgelist), 2)]
   childs <- out$edgelist[seq(2, length(out$edgelist), 2)]
   edges <- paste(parents, childs, sep= " -> ")
-  thetas <- out$thetas[seq(2, length(out$thetas))]
+  thetas <- unlist(inferTheta(data, out)$thetas)
+
   dbn_out <- data.frame(From=parents, To=childs, 
                         Edges=edges, Theta=thetas)
   
@@ -38,4 +36,28 @@ do_DBN <- function(data){
               ))
 }
 
+
+inferTheta <- function(df, fit){
+  ## Copied from OncoDB package https://github.com/phillipnicol/OncoBN
+  df <- cbind(df, 1)
+  colnames(df)[ncol(df)] <- "WT" 
+  
+  g <- make_directed_graph(fit$edgelist) 
+  parent_list <- list()
+  theta_list <- list()
+  for(i in 1:(ncol(df) - 1)) {
+    parents <- incident(g, colnames(df)[i], mode = "in")
+    parent_list[[i]] <- ends(g, parents)[,1]
+    
+    p_on <- as.data.frame(df[, parent_list[[i]] ])
+    theta_list[[i]] <- length(which(df[rowSums(p_on) > 0, i] == 1))/nrow(as.data.frame(p_on[rowSums(p_on) > 0, ]))
+    if(theta_list[[i]] > 1) {
+      stop(theta_list[[i]])
+    }
+  }
+  out <- list()
+  out$parents <- parent_list
+  out$thetas <- theta_list
+  return(out)
+}
 
