0db6f69c38bb5479628b6a62fb1ea97148b9c021
PabloHNieto <pablo.h.nieto@gmail.com>
2021-04-29 11:22:14 +0200

    Changes to match statndar output of similar functions

--- code_from_what_genotype_next/dbn-process.R
+++ code_from_what_genotype_next/dbn-process.R
@@ -11,24 +11,7 @@ do_DBN <- function(data){
   browser()
   dbn_out <- data.frame(From=parents, To=childs, 
                         Edges=edges, Theta=thetas)
-  return(dbn_out)
+  return(list(edges=dbn_out))
 }
 
-# 
-# N <- 50
-# na <- N
-# nb <- N + round( 10 * runif(1))
-# nab <- .6 * N + round( 10 * runif(1))
-# n00 <- round( 10 * runif(1))
-# dB <- matrix(
-#   c(
-#     rep(c(1, 0), na) 
-#     , rep(c(0, 1), nb)
-#     , rep(c(1, 1), nab)
-#     , rep(c(0, 0), n00)
-#   ), ncol = 2, byrow = TRUE
-# )
-# colnames(dB) <- LETTERS[1:2]
-# 
-# z <- fitCPN(dB, model="DBN", algorithm="DP")
 
