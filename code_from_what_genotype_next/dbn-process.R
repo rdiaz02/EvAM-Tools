c61da9eb3ce1e8642e374934e76a4f1ba04a7647
PabloHNieto <pablo.h.nieto@gmail.com>
2021-04-29 10:53:57 +0200

    Intial process of DBN, it requires a modified version on OncoDB to return thetas

--- code_from_what_genotype_next/dbn-process.R
+++ code_from_what_genotype_next/dbn-process.R
@@ -2,9 +2,16 @@ library(OncoBN)
 
 
 do_DBN <- function(data){
-  
-  out <- fitCPN(data)
-  out 
+  out <- fitCPN(data, algorithm="GA")
+  out$edgelist <- replace(out$edgelist, out$edgelist=="WT", "Root")
+  parents <- out$edgelist[seq(1, length(out$edgelist), 2)]
+  childs <- out$edgelist[seq(2, length(out$edgelist), 2)]
+  edges <- paste(parents, childs, sep= " -> ")
+  thetas <- out$thetas[seq(2, length(out$thetas))]
+  browser()
+  dbn_out <- data.frame(From=parents, To=childs, 
+                        Edges=edges, Theta=thetas)
+  return(dbn_out)
 }
 
 # 
