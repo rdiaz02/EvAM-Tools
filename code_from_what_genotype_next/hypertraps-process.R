91a68f71038a69ed8ea764c720fd522e0df6b8be
PabloHNieto <pablo.h.nieto@gmail.com>
2021-05-11 16:42:34 +0200

    Modifying system calls to include full numbers instead of scientific notation

--- code_from_what_genotype_next/hypertraps-process.R
+++ code_from_what_genotype_next/hypertraps-process.R
@@ -5,18 +5,21 @@
 do_HyperTraPS <- function(data, tmp_folder="", runs=1000, bi=50000, r=100, seed=42, conda_env_name="HyperTraPS"){
   ## Create tmp folder with random nameS
   dateTime <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
-  random_letters <- paste(c("_", tmp_folder, "_", LETTERS[floor(runif(4, min=1, max=26))]), sep="", collapse="")
-  tmp_folder <- paste(c("/tmp/", dateTime, random_letters), collapse="")
+  random_letters <- paste(c("_", tmp_folder, "_", LETTERS[floor(runif(4, min=1, max=26))]), collapse="")
 
-  dir.create(tmp_folder)
+  tmp_folder_name <- paste(c(dateTime, random_letters), collapse="")
+  tmp_folder <- file.path("/tmp", conda_env_name, tmp_folder_name), collapse="")
+
+  dir.create(tmp_folder, recursive=TRUE)
   orig_folder <- getwd()
   setwd(tmp_folder)
   print(tmp_folder)
 
   ## Activating conda env
   system(sprintf("conda activate %s", conda_env_name))
+
   ## Running HyperTraps
-  output_name <- "data.csv"
+  output_name <- "data_second_run.csv"
   write.csv(data, output_name)
 
   ### Transforming data
@@ -29,15 +32,15 @@ do_HyperTraPS <- function(data, tmp_folder="", runs=1000, bi=50000, r=100, seed=
   print("Sampling posterior")
   time_posterior <- system.time(
     # system("RUN_MCMC_SAMPLER -f transitions.txt -M second-order -N 1000 -r 20 -n zero -p 20 -k mcmc-apm -s 0.025 -b 50000 -i 100 -t 4 -q 0")
-    system(sprintf("RUN_MCMC_SAMPLER -f transitions.txt -M second-order -N %s -b %s -r %s -S %s", runs, bi, r, seed))
+    system(sprintf("RUN_MCMC_SAMPLER -f transitions.txt -M second-order -N %1.f -b %1.f -r %1.f -S %1.f", runs, bi, r, seed))
   )["elapsed"]
   cat("Elapsed time for sampling posterior ", time_posterior, "\n")
 
   ### Run walkers
   print("Generating Paths")
-  system(sprintf("RUN_PW -w match-data -b %s -R 100", bi))
+  system(sprintf("RUN_PW -w match-data -b %1.f -R 100", bi))
 
-  system(sprintf("plot_mc_stats.py -b %s", bi))
+  system(sprintf("plot_mc_stats.py -b %1.f", bi))
   setwd(orig_folder)
   # Cleaning 
   return(time_posterior)
