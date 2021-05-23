8b215e1f83bf03199100215dee004c26146fece2
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2021-05-23 18:43:54 +0200

    ? this was missing from previous commit?

--- examples/access_genots_from_oncosimul.R
+++ examples/access_genots_from_oncosimul.R
@@ -113,6 +113,66 @@ cpm_to_fitness_genots <- function(x, max_f = 3, max_genots = 2^15) {
 }
 
 
+## named vector of genotype fitness -> fitness graph and transition matrix and
+##                                     accessible genotypes
+##   We assume WT is 1 if not given explicitly
+##   BEWARE: only tested with one minimal test, and by comparing against CBN and OT!! 
+genots_2_fgraph_and_trans_mat <- function(x) {
+    ## Logic:
+    ##  - Construct a fully connected fitness graph
+    ##    between the given genotypes. So any genotype connected to
+    ##    genotypes with one extra mutation.
+    ##  - Construct matrix of fitness differences between ancestor and immediate
+    ##    descendants. Likely slow if many genotypes.
+    ##  - Set to non accessible if fitness difference < 0
+
+    ##  Could be done faster, by not creating the unrestricted fitness graph
+    ##  and instead maybe using OncoSimulR's wrap_accessibleGenotypes
+    ##  But would need to check that works with partial lists of genotypes
+    ##  and use allGenotypes_to_matrix.
+    
+    which_wt <- which(names(x) == "WT")
+    if(length(which_wt) == 1) {
+        fit_wt <- x["WT"]
+        if(fit_wt != 1.0) message("Your WT has fitness different from 1.",
+                                  " Using WT with the fitness you provided.")
+        x <- x[-which_wt]
+    } else {
+        fit_wt <- c("WT" = 1.0)
+    }
+    
+    ## Silly? Inside the next, we now put them together. FIXME?
+    access_genots_as_list <- lapply(names(x),
+                                    function(v) strsplit(v, ", ")[[1]])
+    ## For ordered output
+    no <-  order(unlist(lapply(access_genots_as_list, length)), names(x))
+    access_genots_as_list <- access_genots_as_list[no]
+    
+    fgraph <- unrestricted_fitness_graph_sparseM(access_genots_as_list)
+    
+    genots_fitness <- c(fit_wt, x)[colnames(fgraph)]
+    mf <- matrix(rep(genots_fitness, nrow(fgraph)),
+                 nrow = nrow(fgraph), byrow = TRUE)
+    stopifnot(identical(dim(mf), dim(fgraph)))
+
+    fdiff <- mf - genots_fitness
+    fdiff <- fgraph * fdiff
+
+    fgraph[fdiff < 0] <- 0
+    tm <- fdiff
+    tm[tm < 0] <- 0
+    tm <- tm / ifelse(rowSums(tm) != 0, rowSums(tm), 1)
+    ## This we know is always 0
+    tm[nrow(tm), ] <- 0
+    ## This we can set
+    tm[rowSums(fgraph) == 0, ] <- 0
+    return(list(fitness_graph = fgraph,
+           transition_matrix = tm,
+           fitness_differences = fdiff,
+           accessible_genotypes = colnames(fgraph)[colSums(fgraph) >= 1]))
+
+}
+
 ## output from CPMs, min and max final fitness, max num genots ->
 ##                            list(accessible genotypes ,
 ##                                 fitness graph,
@@ -120,35 +180,22 @@ cpm_to_fitness_genots <- function(x, max_f = 3, max_genots = 2^15) {
 ##    max_f and min_f should have no effect in probs transition
 ##    accessible_genotypes: gives the genotypes and their fitness,
 ##                          computed from the lambdas (thetas) and possibly scaled
-cpm_to_trans_mat_oncosimul <- function(x, max_f = 3,
+cpm_to_trans_mat_oncosimul <- function(x, max_f = NULL,
                                        max_genots = 2^15) {
     fitness_all <- cpm_to_fitness_genots(x, max_f = max_f,
                                          max_genots = max_genots)
 
     access_genots_fitness <- fitness_all$Fitness[fitness_all$Fitness > 1.0]
     names(access_genots_fitness) <- fitness_all$Genotype[fitness_all$Fitness > 1.0]
-    
-    ## Silly? Inside the next, we now put them together. FIXME?
-    access_genots_as_list <- lapply(names(access_genots_fitness),
-                                    function(v) strsplit(v, ", ")[[1]])
 
-    fgraph <- unrestricted_fitness_graph_sparseM(access_genots_as_list)
-    
-    ## Not very efficient, but works
-    o_access_genots_fitness <- c("WT" = 1, access_genots_fitness)[colnames(fgraph)]
-    mf <- matrix(rep(o_access_genots_fitness, nrow(fgraph)),
-                 nrow = nrow(fgraph), byrow = TRUE)
-    ## As difference w.r.t. original genotype
-    mf <- mf - o_access_genots_fitness
-    tm <- fgraph * mf
-    tm_s <- tm / rowSums(tm)
-    tm_s[nrow(tm_s), ] <- 0
-    return(list(accessible_genotypes = access_genots_fitness,
-                fitness_graph = fgraph,
+    gfo <- genots_2_fgraph_and_trans_mat(access_genots_fitness)
+
+    return(list(accessible_genotypes = gfo$accessible_genotypes,
+                fitness_graph = gfo$fitness_graph,
                 ## For checking; these are not necessarily the original lambdas
-                ## but a the original multiplied by a factor, like changing the rate of all.
-                ## lambdas = tm, 
-                transition_matrix = tm_s
+                ## but the original multiplied by a factor, like changing the rate of all.
+                ## lambdas = gfo$fitness_differences, 
+                transition_matrix = gfo$transition_matrix
                 ))
 }
 
