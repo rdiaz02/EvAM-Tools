9822cfe14cb309ceafc93f0bfa7f4c0a337e0b2c
PabloHNieto <pablo.h.nieto@gmail.com>
2021-05-27 18:09:42 +0200

    addin dry-run option to avoid reruning and just generate the plots

--- code_from_what_genotype_next/hypertraps-process.R
+++ code_from_what_genotype_next/hypertraps-process.R
@@ -1,22 +1,27 @@
 ## Set up: add PATH_TO_HYPERTRAPS_REPO/src/python to your PATH
 ## Set up: add PATH_TO_HYPERTRAPS_REPO/bin/ to your PATH
 ## Set up: operate in a conda environment: Follow installation in README.md
+library(reticulate) ## To activate the conda environment
 
-do_HyperTraPS <- function(data, tmp_folder="", runs=1000, bi=50000, r=100, seed=42, conda_env_name="HyperTraPS"){
-  ## Create tmp folder with random nameS
-  dateTime <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
-  random_letters <- paste(c("_", tmp_folder, "_", LETTERS[floor(runif(4, min=1, max=26))]), collapse="")
+do_HyperTraPS <- function(data, tmp_folder="", 
+  runs=1000, bi=50000, r=100, 
+  seed=0, conda_env_name="HyperTraPS", dry_run=F){
+  ## Create tmp folder with random name
+  if(tmp_folder == "") {
+    dateTime <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
+    random_letters <- paste(c("_", 
+      LETTERS[floor(runif(4, min = 1, max = 26))]), collapse = "")
+    tmp_folder_name <- paste(c(dateTime, random_letters), collapse = "")
+    tmp_folder <- file.path("/tmp", conda_env_name, tmp_folder_name)
+  }
 
-  tmp_folder_name <- paste(c(dateTime, random_letters), collapse="")
-  tmp_folder <- file.path("/tmp", conda_env_name, tmp_folder_name)
-
-  dir.create(tmp_folder, recursive=TRUE)
+  dir.create(tmp_folder, recursive = TRUE)
   orig_folder <- getwd()
   setwd(tmp_folder)
   print(tmp_folder)
 
   ## Activating conda env
-  # system(sprintf("conda activate %s", conda_env_name))
+  use_condaenv(conda_env_name)
 
   ## Running HyperTraps
   output_name <- "data.csv"
@@ -29,19 +34,26 @@ do_HyperTraPS <- function(data, tmp_folder="", runs=1000, bi=50000, r=100, seed=
 
   ### MCM sampler
   ## TODO check the meaning of all parameters and allow to set them
-  print("Sampling posterior")
-  time_posterior <- system.time(
-    # system("RUN_MCMC_SAMPLER -f transitions.txt -M second-order -N 1000 -r 20 -n zero -p 20 -k mcmc-apm -s 0.025 -b 50000 -i 100 -t 4 -q 0")
-    system(sprintf("RUN_MCMC_SAMPLER -f transitions.txt -M second-order -N %1.f -b %1.f -r %1.f -S %1.f", runs, bi, r, seed))
-  )["elapsed"]
-  cat("Elapsed time for sampling posterior ", time_posterior, "\n")
+  if(!dry_run){
+    print("Sampling posterior")
+    time_posterior <- system.time(
+      system(sprintf("RUN_MCMC_SAMPLER -f transitions.txt -M second-order -N %1.f -b %1.f -r %1.f -S %1.f", runs, bi, r, seed))
+    )["elapsed"]
+    cat("Elapsed time for sampling posterior ", time_posterior, "\n")
+  }
 
   ### Run walkers
   print("Generating Paths")
   system(sprintf("RUN_PW -w match-data -b %1.f -R 100", bi))
+  system(sprintf("RUN_PW -w zero-one -b %1.f -R 100", bi))
 
   ### Plots
   system(sprintf("plot_mc_stats.py -b %1.f", bi))
+  system(sprintf("plot_hypercube_graph.py -f 'forwards_list-pord-match-data.csv' -outfile_graph 'forwards-hypercube-graph-mach-data-g0' -transition_data 'transitions.txt' -labels 'labels.csv' -label_type 'greedy_data' -labels_fontsize 4 -layout_type 'spring' -labels 'labels.csv' -aspect 0.9 -width 3.4 -out_type 'png'"))
+    
+  system(sprintf("plot_feature_graph.py -f 'forwards.txt' -prob_type 'joint' -layout_type 'circular' -data_type 'match-data' -width 4 -fontsize 10 -any_time 0 -node_size 100 -connection_style 'arc3,rad=-0.3' -outfile_type 'png'"))
+
+  system(sprintf("plot_ordering_histogram.py -f 'forwards_list-pord-zero-one.csv' -f2 'forwards_list-pord-match-data.csv' -transition_data 'transitions.txt' -labels 'labels.csv' -fontsize 6 -xevery 1 -aspect 0.9 -verbose 'no' -outfile 'forwards-ws1-ws2' -out_type 'png'"))
 
   ### Extract data from the model:
   ## Posets and transitions numbers
--- examples/hyperTraPS_AND_OR_XOR.R
+++ examples/hyperTraPS_AND_OR_XOR.R
@@ -6,33 +6,18 @@ rm(pwd0)
 
 # dB_AND <- matrix(
 #   c(
-#     rep(c(1, 0, 0), 100)
-#     , rep(c(1, 1, 0), 100)
-#     , rep(c(1, 1, 1), 100)
+#     rep(c(1, 0, 0, 0), 100)
+#     , rep(c(1, 1, 0, 0), 100)
+#     , rep(c(1, 1, 1, 0), 100)
+#     , rep(c(1, 1, 1, 1), 100)
 #     , rep(c(0, 0, 0), 10)
-#   ), ncol = 3, byrow = TRUE
+#   ), ncol = 4, byrow = TRUE
 # )
-# colnames(dB_AND) <- LETTERS[1:3]
+# colnames(dB_AND) <- LETTERS[1:4]
 
-do_HyperTraPS(dB_AND, "HP_AND", runs = 500, bi=2000)
+# do_HyperTraPS(dB_AND, "HP_AND", runs = 500, bi=200)
 
-dB_OR <- matrix(
-  c(
-    rep(c(1, 0, 0, 0), 200)
-    , rep(c(1, 0, 1, 0), 100)
-    , rep(c(1, 1, 0, 0), 100)
-    , rep(c(1, 1, 1, 0), 50)
-    , rep(c(1, 1, 0, 1), 50)
-    , rep(c(1, 0, 1, 1), 50)
-    , rep(c(1, 1, 1, 1), 10)
-    , rep(c(0, 0, 0, 0), 10)
-  ), ncol = 4, byrow = TRUE
-)
-colnames(dB_OR) <- LETTERS[1:4]
-
-do_HyperTraPS(dB_OR, "HP_OR", runs = 500, bi=2000)
-
-# dB_XOR <- matrix(
+# dB_OR <- matrix(
 #   c(
 #     rep(c(1, 0, 0, 0), 200)
 #     , rep(c(1, 0, 1, 0), 100)
@@ -44,6 +29,20 @@ do_HyperTraPS(dB_OR, "HP_OR", runs = 500, bi=2000)
 #     , rep(c(0, 0, 0, 0), 10)
 #   ), ncol = 4, byrow = TRUE
 # )
-# colnames(dB_XOR) <- LETTERS[1:4]
+# colnames(dB_OR) <- LETTERS[1:4]
 
-# do_HyperTraPS(dB_XOR, "HP_XOR", runs = 500, bi=2000)
+# do_HyperTraPS(dB_OR, "HP_OR", runs = 500, bi=200)
+
+dB_XOR <- matrix(
+  c(
+    rep(c(1, 0, 0, 0), 200)
+    , rep(c(1, 1, 0, 0), 100)
+    , rep(c(1, 0, 1, 0), 100)
+    , rep(c(1, 1, 0, 1), 50)
+    , rep(c(1, 0, 1, 1), 50)
+    , rep(c(0, 0, 0, 0), 10)
+  ), ncol = 4, byrow = TRUE
+)
+colnames(dB_XOR) <- LETTERS[1:4]
+
+do_HyperTraPS(dB_XOR, "HP_XOR", runs = 500, bi=200, dry_run=T)
