a66633ddef072fbf8f1a895c5bcb35f8e8c81ac6
PabloHNieto <pablo.h.nieto@gmail.com>
2021-07-08 19:47:20 +0200

    New script for chi-square test

--- examples/chi_square_simulations_distributions.R
+++ examples/chi_square_simulations_distributions.R
@@ -14,49 +14,79 @@ source("../data/toy_datasets.R")
 setwd(pwd0)
 rm(pwd0)
 
-K <- 20 # Random scenarios
-M <- 320 # Number of replicate
-# M <- 2 # Number of replicate
+K <- 100 # Random scenarios
+M <- 3200 # Number of replicate
+# M <- 8 # Number of replicate
 N <- 100000 # Number of simulated samples
 # N <- 100 # Number of simulated samples
-p_values_1 <- matrix(0, K, M)
+pval <- matrix(0, K, M)
 p_values_2 <- matrix(0, K, M)
-
+n_genes <- rep(0, K)
+all_results <- list(rep(0, K * M))
 fun_1 <- function(trm){
     sim <- simulate_population(trm, N)
     freqs <- process_simulations(sim, output = c("frequencies"))$frequencies
 
     ordering <- order(vapply(freqs$Genotype, str2int, numeric(1)))
     chi_sq_test_1 <- chisq.test(freqs$Counts[ordering], p = p_distribution)
-    # p_values_1[k, m] <- chi_sq_test$p.value
-    chi_sq_test_2 <- chisq.test(freqs$Counts[ordering]/sum(freqs$Counts), p = p_distribution)
-    # p_values_2[k, m] <- chi_sq_test$p.value
-    return(c(chi_sq_test_1$p.value, chi_sq_test_2$p.value))
+    # chi_sq_test_2 <- chisq.test(freqs$Counts[ordering]/sum(freqs$Counts), p = p_distribution)
+    # return(c(chi_sq_test_1$p.value, chi_sq_test_2$p.value))
+    return(list(
+        xi1 = chi_sq_test_1$p.value
+        # , xi2 = chi_sq_test_2$p.value
+        , sim = sim
+        , freqs = freqs
+        , ordering = ordering))
 }
-
-for (k in 1:K){
-    ngenes <- round(runif(1, 2, 12))
+idx <- 1
+p_distributions <- list(rep(0, K))
+for (k in 2:5){
+    # ngenes <- round(runif(1, 2, 12))
+    ngenes <- k
     Theta.true <- Random.Theta(n = ngenes, sparsity = 0.50)
     rownames(Theta.true) <- LETTERS[1:ngenes]
     colnames(Theta.true) <- LETTERS[1:ngenes]
     
     p_distribution <- Generate.pTh(Theta.true)
-    
+    p_distributions[[k - 1]] <- p_distribution
     transition_rate_matrix <- theta_to_trans_rate_3(Theta.true)
-    print(sprintf("Sample %s", k))
-    p_values <- mclapply(list(transition_rate_matrix, transition_rate_matrix)
-        , fun_1
-        # , MoreArgs = list(trm = transition_rate_matrix)
+    print(sprintf("N genes %s", k))
+    p_values <- mclapply(sapply(1:M, function(x) list(x = transition_rate_matrix))
+        , fun_1, mc.cores = 32
     )
-    p_values <- matrix(unlist(p_values), ncol = 2, byrow = TRUE)
+    # browser()
+    # p_values <- matrix(unlist(p_values), ncol = 2, byrow = TRUE)
 
-    p_values_1[k, ] <- p_values[, 1]
-    p_values_2[k, ] <- p_values[, 2]
-    saveRDS(p_values_1, "p_values_1.rds")
-    saveRDS(p_values_2, "p_values_2.rds")
+    pval[(k - 1), ] <- as.vector(vapply(p_values, function(x)x$xi1, numeric(1.0)))
+    I <- length(p_values)
+    for (i in 1:I) {
+        print(idx)
+        all_results[[idx]] <- p_values[[i]]
+        idx <- idx + 1
+    }
+    # all_results[ (k - 1), ] <- list(xi1 = p_)
+    # p_values_2[k, ] <- as.vector(vapply(p_values, function(x)x$xi2), numeric(1.0))
+    # n_genes[k] <- ngenes
+    saveRDS(p_val, "p_val.rds")
+    # saveRDS(p_values_2, "p_values_2.rds")
 }
 
+report <- function(idx, ngenes){
+    print(sprintf("N genes %s", ngenes))
+    idx_2 <- ngenes - 1
+    # print(pval[idx_2, ])
+    x <- all_results[[(idx_2-1) * 8 + idx]]
+    x$freqs["Analytical"] <- floor(p_distributions[[idx_2]] * N)[x$ordering]
+    x$freqs["Diff"] <- x$freqs$Counts - x$freqs$Analytical
+    x$freqs["Percent"] <- x$freqs$Diff/x$freqs$Analytical
+    print(x$freqs[order(abs(x$freqs$Percent)),])
+    print(x$xi1)
+    print(sum(x$freqs$Diff))
+}
 
-
+report(1, 5)
+report(1, 4)
+report(1, 3)
+report(1, 2)
 
 
