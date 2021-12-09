426acfbaf1290240d8a92165de6f0804025b852e
PabloHNieto <pablo.h.nieto@gmail.com>
2021-12-09 11:40:12 +0100

    Cleaning stuff

--- guloMAM/insts/miscell/code_to_biol_examples/compute_cpm_from_toy_data_sets.R
+++
@@ -1,9 +0,0 @@
-# source("../data/toy_datasets.R")
-# source("access_genots_from_oncosimul.R")
-
-for (i in names(all_examples)){
-    print(sprintf("Dataset %s", i))
-    tmp_dataset <- all_examples[[i]]
-    out <- all_methods_2_trans_mat(tmp_dataset)
-    saveRDS(out, sprintf("../data/toy_datasets_cpms/%s.rds", i))
-}
