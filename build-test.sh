a0e263761871d78bdae6ea22e3267ad711163df3
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-01-11 13:27:56 +0100

    FIXMEs in build-test.sh.

--- build-test.sh
+++ build-test.sh
@@ -8,3 +8,7 @@ R_ENVIRON_USER=~/.Renviron.bioc R CMD build evamtools
 R_ENVIRON_USER=~/.Renviron.bioc R CMD check evamtools_0.0.0.9015.tar.gz
 
 R_ENVIRON_USER=~/.Renviron.bioc R CMD INSTALL evamtools_0.0.0.9015.tar.gz
+
+## FIXME:
+##    FIXME:   R version should be at least 4.1.2
+##    FIXME:   but do we really need to export R_LIBS_SITE?
