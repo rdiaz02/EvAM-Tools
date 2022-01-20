1f101143d04a6ceb42377ecbcefe473853c6cebf
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-01-20 20:39:23 +0100

    oooops, this file was not in the repo

---
+++ evamtools/R/non-exported-from-others.R
@@ -0,0 +1,33 @@
+## Avoid use of ":::". Instead, use approach in
+## https://stackoverflow.com/a/46098814
+
+evam_allGenotypes_to_matrix <-
+    utils::getFromNamespace("allGenotypes_to_matrix",
+                            "OncoSimulR")
+
+evam_wrap_accessibleGenotypes <-
+    utils::getFromNamespace("wrap_accessibleGenotypes",
+                            "OncoSimulR")
+
+evam_checkProperOTAdjMat <- utils::getFromNamespace("checkProperOTAdjMat",
+                                                    "OncoSimulR")
+
+evam_checkProperMinimalAdjMat <-
+    utils::getFromNamespace("checkProperMinimalAdjMat",
+                            "OncoSimulR")
+
+
+evam_OTtoPoset <- utils::getFromNamespace("OTtoPoset",
+                                          "OncoSimulR")
+
+evam_posetToGraph <- utils::getFromNamespace("posetToGraph",
+                                             "OncoSimulR")
+
+evam_shannonI <- utils::getFromNamespace("shannonI",
+                                         "OncoSimulR")
+
+evam_inferTheta <- utils::getFromNamespace("inferTheta", "OncoBN")
+
+
+
+
