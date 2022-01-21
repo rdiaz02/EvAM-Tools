47660781b6af1044bce95e0bed42544dd4266322
PabloHNieto <pablo.h.nieto@gmail.com>
2022-01-21 20:32:07 +0100

    updating simple_test to also run MCCBN

--- evamtools/inst/miscell/examples/simple_test.R
+++ evamtools/inst/miscell/examples/simple_test.R
@@ -21,15 +21,15 @@ colnames(dB_c1) <- LETTERS[1:5]
 
 
 # out_with_simulations <- readRDS("cpm_out_with_simulations.rds")
-out <- all_methods_2_trans_mat(dB_c1)
+out <- all_methods_2_trans_mat(dB_c1, do_MCCBN=TRUE)
 # do_HyperTraPS(dB_AND, "HyperTraPS_examples/HP_AND", runs = 500, bi=200, dry_run = TRUE)
 # plot_DAG_fg(out, dB_c1, plot_type = "matrix")
 
-out_with_simulations <- sample_all_CPMs(out, 100000, 5)
-saveRDS(out_with_simulations, "cpm_out_with_simulations.rds")
+out_with_simulations <- sample_all_CPMs(out, 10000, 5)
+#saveRDS(out_with_simulations, "cpm_out_with_simulations.rds")
 # png("graph.png", width = 1000, height = 600, units = "px")
 # plot_DAG_fg(out_with_simulations, dB_c1, plot_type = "genotypes")
 # dev.off()
-# png("transitions.png", width = 1000, height = 600, units = "px")
-# plot_DAG_fg(out_with_simulations, dB_c1, plot_type = "transitions", top_paths = 4)
-# dev.off()
+ png("transitions.png", width = 1000, height = 600, units = "px")
+ plot_DAG_fg(out_with_simulations, dB_c1, plot_type = "transitions", top_paths = 4)
+ dev.off()
