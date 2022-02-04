7c6a6f8a6529e35bcf19dca59631214c73b4e86b
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-02-04 13:46:56 +0100

    use a proper test

--- evamtools/tests/testthat/test.exercise-main-funct.R
+++ evamtools/tests/testthat/test.exercise-main-funct.R
@@ -1,7 +1,10 @@
-data(every_which_way_data)
-Dat1 <- every_which_way_data[[16]][1:40, 2:6]
-null <- evam(Dat1,
-             methods = c("CBN", "OT",
-                         "MHN", "HESBCN",
-                         "DBN"))
+test_that("Minimal test: we can run", {
+    data(every_which_way_data)
+    Dat1 <- every_which_way_data[[16]][1:40, 2:6]
+    null <- expect_output(suppressMessages(evam(Dat1,
+                                  methods = c("CBN", "OT",
+                                              "MHN", "HESBCN",
+                                              "DBN"))))
+})
+
 cat("\n Done test.exercise-main-funct.R \n")
