1112eb2efb8ed5987c26ad6e48fcfa630d1badf2
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-02-06 21:06:37 +0100

    more robust testing
    - skip absurd cases for testing

--- other_miscell/test.run_hesbcn_continuously.R
+++ other_miscell/test.run_hesbcn_continuously.R
@@ -41,19 +41,37 @@ while(TRUE) {
     }
 
     run_test_for_dataset <- function(data) {
-        expect_equal(
-            reorder_trans_mat(evamtools:::cpm2tm(data$edges, max_f = NULL)$transition_matrix),
+        ## Used in several places. Run just once
+        cpm2_out <- evamtools:::cpm2tm(data$edges, max_f = NULL)
+        if(min(data$edges$Lambda) < 1e-15) {
+            warning("Skipping comparison with OncoSimul's ",
+                    "transition rate matrix: ",
+                    "smallest lambda < 1e-15")
+            ## We compute products of numbers close to R's smallest limit.
+            ## We are probably fine with numbers even as small as 2e-16, but to be safe.
+        } else {
+            expect_equal(
+            reorder_trans_mat(cpm2_out$transition_matrix),
             reorder_trans_mat(evamtools:::cpm_access_genots_paths_w_simplified_relationships(
                                               data)$trans_mat_genots),
             check.attributes = TRUE)
+        }
 
         ## Do not run next test if fitness is absurdly large as scaling will fail
-        ## as it should.
-        max_fitness <- max(evamtools:::cpm2tm(data$edges, max_f = NULL)$accessible_genotypes)
-        if(max_fitness < 1e10) {
+        ## (as it should).  Do not run it either if ratio of largest to smallest
+        ## lambda is > 1e9 as in products we accumulate tiny errors that give
+        ## differences of the order of 1e-8.
+        max_fitness <- max(cpm2_out$accessible_genotypes)
+        ratio_lambdas <- max(data$edges$Lambdas)/min(data$edges$Lambdas)
+        if((max_fitness < 1e10) && (ratio_lambdas < 1e9))  {
             maxff <- sample(c(2, 3, 3.5, 3.8, 4, 5, 8), size = 1)
-            expect_equal(as.matrix(evamtools:::cpm2tm(data$edges, max_f = NULL)$transition_matrix),
+            expect_equal(as.matrix(cpm2_out$transition_matrix),
                          as.matrix(evamtools:::cpm2tm(data$edges, max_f = maxff)$transition_matrix))
+        } else {
+            warning("Skipping comparison of transition matrices with ",
+                    "different fitness scaling",
+                    ifelse(max_fitness >= 1e10, ". max_fitness >= 1e10", ""),
+                    ifelse(ratio_lambdas >= 1e9, ". Ratio of Lambdas >= 1e9"))
         }
     }
     
@@ -170,3 +188,12 @@ while(TRUE) {
     iters <- iters + 1
     s <- s + 1
 }
+
+
+### Eh!!! This crashes!!!
+### Seed =  531589
+### Check but it is a simple numerical issue of a lambda 15 orders of magnitude smaller than the rest.
+## I think it is because OncoSimulR thresholds at fitness < 1e-9 or something.
+## Add it to tests
+
+## Seed =  589375 
