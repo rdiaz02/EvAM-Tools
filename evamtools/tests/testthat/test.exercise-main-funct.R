40b513493f8b21c1bb7dfebd25b933eda1853d9f
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-02-09 21:33:15 +0100

    testing of main funct with weird gene names

--- evamtools/tests/testthat/test.exercise-main-funct.R
+++ evamtools/tests/testthat/test.exercise-main-funct.R
@@ -58,6 +58,35 @@ test_that("We can deal with duplicated columns and columns without events and co
     exercise_sample_all_CPMs(out3)
     exercise_sample_all_CPMs(out4)
     exercise_sample_all_CPMs(out5)
+
+    ## Some examples with random, weird names
+    iters <- 10
+    dd <- Dat1
+    for(i in 1:iters) {
+        ngenes <- ncol(dd)
+        gn <- vector(mode = "character", length = ngenes)
+
+        ## create weird gene names
+        for(g in seq_len(ngenes)) {
+            l1 <- sample(c(LETTERS, letters), 1)
+            rest <-  c(sample(
+                c(
+                    sample(c("_",  "=", "?", "#", "@", "%", "&", "!"), 4, replace = TRUE)
+                    , sample(0:9, 4, replace = TRUE)
+                    , sample(c(letters, LETTERS), 4, replace = TRUE)
+                )))
+            
+            rest <- paste(rest, sep = "", collapse = "")
+            gn[g] <- paste0(l1, rest)
+        }
+        colnames(dd) <- gn
+        outdd <- suppressMessages(evam(dd,
+                                  methods = c("CBN", "OT", "OncoBN",
+                                              "MHN", "HESBCN"),
+                                  max_cols = 4))
+        expect_true(exists("OT_model", where = outdd))
+        exercise_sample_all_CPMs(outdd)
+    }
 })
 
 
