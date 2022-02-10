bbd234c33cd0debc4442aa580e86cd5842944177
PabloHNieto <pablo.h.nieto@gmail.com>
2022-02-10 20:58:11 +0100

    Removing commented sections

--- evamtools/inst/miscell/examples/test.plotting.R
+++ evamtools/inst/miscell/examples/test.plotting.R
@@ -52,29 +52,3 @@ dev.off()
 png("3_4_some_horizontal_obs_trans.png", width = 1000, height = 600, units = "px")
 plot_CPMs(sample_evam_output, samples=samples, models=c("OT", "CBN"), orientation="horizontal", plot_type = "transitions", fixed_vertex_size=FALSE)
 dev.off()
-
-# png("2_all_vertical.png", width = 600, height = 1000, units = "px")
-# plot_CPMs(sample_evam_output, orientation="vertical")
-# dev.off()
-
-# #Checking models to plot
-# png("3_some_horizontal.png", width = 1000, height = 600, units = "px")
-# plot_CPMs(sample_evam_output, models = c("OT", "CBN", "OncoBN"), orientation="horizontal")
-
-# png("4_some_vertical.png", width = 600, height = 1000, units = "px")
-# plot_CPMs(sample_evam_output, models = c("OT", "CBN", "OncoBN"),orientation="vertical")
-
-# #Checking type of plot
-# png("5_some_horizontal_prob.png", width = 1000, height = 600, units = "px")
-# plot_CPMs(sample_evam_output, models = c("MHN", "CBN", "OncoBN"), orientation="horizontal", plot_type="probabilities")
-# dev.off()
-
-# png("6_some_vertical_trm.png", width = 600, height = 100, units = "px")
-# plot_CPMs(sample_evam_output, models = c("OT", "CBN", "OncoBN", "CBN"),orientation="vertical", plot_type="trm")
-# dev.off()
-
-# png("7_all_horizontal_trans.png", width = 1000, height = 600, units = "px")
-# plot_CPMs(sample_evam_output,orientation="horizontal", plot_type="transitions")
-# dev.off()
-
-# # samples <- evamtools:::sample_all_CPMs(sample_evam_output, 100)
