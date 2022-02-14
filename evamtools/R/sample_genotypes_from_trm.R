e2fe0286f00cdbd87431762ae575db44c55aa181
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-02-14 19:07:08 +0100

    done coding and testing probs directly from trm

--- evamtools/R/sample_genotypes_from_trm.R
+++ evamtools/R/sample_genotypes_from_trm.R
@@ -501,7 +501,17 @@ canonicalize_genotype_names <- function(x) {
     return(pasted_sorted)
 }
 
-
+## Genotypes in what for me is their "standard, sensible, order"
+## By number of mutations, and within number of mutations, ordered
+## as given by order.
+genotypes_standard_order <- function(gene_names) {
+    gene_names <- sort(gene_names)
+    allgt <- allGenotypes_3(length(gene_names))$mutated
+    gtn <- vapply(allgt, function(v) paste(gene_names[v], collapse = ", "),
+                  "")
+    gtn[1] <- "WT"
+    return(gtn)
+}
 
 ## Obtain probabilities of genotypes from transition rate matrix
 ## under sampling time distributed as exponential rate 1.
@@ -513,8 +523,6 @@ canonicalize_genotype_names <- function(x) {
 ##    Assumptions:
 ##     - x is a sparse matrix
 ##     - First column/row of x is WT
-##     - Last column/row of x is WT with all loci mutated
-##     - x contains all genotypes (i.e., 2^number of genes)
 ##     - For now, the initial distribution is 100% are WT
 ##          (could change, ensuring genotype order matches)
 
@@ -523,24 +531,13 @@ canonicalize_genotype_names <- function(x) {
 
 ## Yes, this is much slower, like two orders of magnitude,
 ## than Schill's Generate.pTh. Still, about 0.3 to 0.4 seconds
-## for 11 genes, and most th1n 90% spent in the checks.
-probs_from_trm <- function(x, p0 = NULL, number_genes = NULL,
-                           tolerance = 10 * sqrt(.Machine$double.eps)) {
-    if(is.null(number_genes)) 
-        number_genes <- stringi::stri_count_fixed(colnames(x)[ncol(x)], ",") + 1
-    if(!is.null(p0))
-        stop("Not yet implemented. ",
-             " If you decide to allow for it ",
-             "make sure genotypes ordered identically.")
-    
-    if (is.null(p0)) p0 <-  c(1, rep(0, (2^number_genes) - 1))
-     
-    stopifnot(nrow(x) == 2^number_genes)
-    stopifnot(ncol(x) == 2^number_genes)
-    stopifnot(length(p0) == nrow(x))
-    stopifnot(isTRUE(all.equal(sum(p0), 1)))
-    
-    if(nnzero(tril(x)))
+## for 11 genes, and most than 90% spent in the checks.
+probs_from_trm <- function(x,
+                           tolerance = 10 * sqrt(.Machine$double.eps),
+                           all_genotypes = TRUE) {
+    p0 <-  c(1, rep(0, nrow(x) - 1))
+
+    if(Matrix::nnzero(tril(x)))
         stop("Lower triangular not 0. Is this transposed?")
     Q <- t(x)
     diag(Q) <- -1 * colSums(Q)
@@ -568,89 +565,24 @@ probs_from_trm <- function(x, p0 = NULL, number_genes = NULL,
         p <- as.vector(p)
         warning("sum(p) - 1  = ", sum(p) - 1)
         }
-    return(p)
+    
+    if(!all_genotypes) return(p)
+   
+    ## Get genes and from them number genotypes and identity genotypes
+    gene_names <- sort(setdiff(unique(unlist(strsplit(colnames(x), split = ", "))),
+                          "WT"))
+    number_genes <- length(gene_names)
+    num_genots <- 2^number_genes
+
+    if(length(p) == num_genots) return(p)
+    
+    allGts <- genotypes_standard_order(gene_names)
+    p_all <- rep(0.0, length = length(allGts))
+    names(p_all) <- allGts
+    p_all[names(p)] <- p
+    return(p_all)
 }
 
 
 
 
-
-
-
-
-
-
-
-## Obtain probabilities of genotypes from transition rate matrix
-## under sampling time distributed as exponential rate 1.
-## 
-## Using equation 4 (p. 243) in Schill et al., 2020, Bioinformatics, 36
-##    "Modelling cancer progression using Mutual Hazard Networks"
-##    and following their code (but using Jacobi from Rlinsolve).
-##
-##    Assumptions:
-##     - x is a sparse matrix
-##     - First column/row of x is WT
-##     - Last column/row of x is WT with all loci mutated
-##     - x contains all genotypes (i.e., 2^number of genes)
-##     - For now, the initial distribution is 100% are WT
-##          (could change, ensuring genotype order matches)
-
-##  The final all.equal uses a tolerance larger than that of
-##  the usual all.equal.
-
-## Yes, this is much slower, like two orders of magnitude,
-## than Schill's Generate.pTh. Still, about 0.3 to 0.4 seconds
-## for 11 genes, and most th1n 90% spent in the checks.
-
-probs_from_trm0 <- function(x, p0 = NULL, number_genes = NULL,
-                           tolerance = 10 * sqrt(.Machine$double.eps)) {
-    if(is.null(number_genes)) 
-        number_genes <- stringi::stri_count_fixed(colnames(x)[ncol(x)], ",") + 1
-    if(!is.null(p0))
-        stop("Not yet implemented. ",
-             " If you decide to allow for it ",
-             "make sure genotypes ordered identically.")
-    
-    if (is.null(p0)) p0 <-  c(1, rep(0, (2^number_genes) - 1))
-     
-    stopifnot(nrow(x) == 2^number_genes)
-    stopifnot(ncol(x) == 2^number_genes)
-    stopifnot(length(p0) == nrow(x))
-    stopifnot(isTRUE(all.equal(sum(p0), 1)))
-    
-    if(nnzero(tril(x)))
-        stop("Lower triangular not 0. Is this transposed?")
-    Q <- t(x)
-    diag(Q) <- -1 * colSums(Q)
-
-    ## Equation 4 in Schill et al. Thus
-    ## (I - Q) * p = p0
-    I_Q <- Matrix::Diagonal(nrow(Q)) - Q
-    p1 <- Rlinsolve::lsolve.jacobi(A = I_Q, B = p0, adjsym = FALSE,
-                                       reltol = 0.0001 * sqrt(.Machine$double.eps),
-                                       verbose = FALSE, weight = 1)
-    ## FIXME
-    ## Occasionally, small differences w.r.t sum. Rerun
-    ## But I should probably use a better method
-    p2 <- Rlinsolve::lsolve.jacobi(A = I_Q, B = p0,
-                                   weight = 1,
-                                   xinit = p1$x,
-                                   adjsym = FALSE,
-                                   reltol = 0.0001 * sqrt(.Machine$double.eps),
-                                   verbose = FALSE)
-    p <- p2$x
-    p <- as.vector(p)
-    names(p) <- colnames(x)
-    if(!isTRUE(all.equal(sum(p), 1, tolerance = tolerance))) {
-        p <- p2$x
-        p <- as.vector(p)
-        warning("sum(p) - 1  = ", sum(p) - 1)
-        }
-    return(p)
-}
-
-
-## For many methods, the transition rate matrix does not contain
-## all genotypes
-## to_full_trm <- 
--- evamtools/tests/testthat/test.probs-from-trm.R
+++ evamtools/tests/testthat/test.probs-from-trm.R
@@ -27,7 +27,7 @@ probs_from_Schill <- function(gene_names) {
     rm(oindex)
     p2 <- evamtools:::Generate.pTh(thetas2)
     names(p2) <- evamtools:::generate_sorted_genotypes(n, gene_names,
-                                                      sort_gene_names = TRUE)
+                                                       sort_gene_names = TRUE)
 
     ## gn_p_can might be the same as names(p) if gene_names was ordered
     gn_p_can <- canonicalize_genotype_names(names(p))
@@ -99,8 +99,8 @@ weird_gene_names <- function(ngenes) {
 
 
 test_that("Same results as original MHN code by Schill", {
-    for(i in 1:10) {
-        ng <- 8
+    for(i in 1:5) {
+        ng <- 5
         compare_schill_evam_probs(LETTERS[1:ng])
         compare_schill_evam_probs(sample(LETTERS[1:ng]))
     }
@@ -111,9 +111,9 @@ test_that("Same results as original MHN code by Schill", {
     ##     compare_schill_evam_probs(LETTERS[1:ng])
     ##     compare_schill_evam_probs(sample(LETTERS[1:ng]))
     ## }
-       
-    for(i in 1:10) {
-        ng <- sample(2:7, 1)
+    
+    for(i in 1:5) {
+        ng <- sample(2:5, 1)
         wg <- weird_gene_names(ng)
         compare_schill_evam_probs(wg)
         compare_schill_evam_probs(wg)
@@ -121,9 +121,115 @@ test_that("Same results as original MHN code by Schill", {
 })
 
 
+if(FALSE) {
+    test_that("Compare the probs obtained with those from sampling", {
+        ## Going down this route is not worth it. This is just an example.
+        ## But these tests would take a long time to code and run for them
+        ## to test things well automatically.
+        ## See the tests for comparing between the sampling and the
+        ## sampling from MC-CBN and MHN
 
-data(every_which_way_data)
-Dat1 <- every_which_way_data[[16]][1:40, 2:6]
-out <- suppressMessages(evam(Dat1,
-                             methods = c("CBN", "MHN", "HESBCN")))
+        data(every_which_way_data)
+        Dat1 <- every_which_way_data[[16]][1:40, 2:6]
+        out <- suppressMessages(evam(Dat1,
+                                     methods = c("CBN", "MHN", "HESBCN")))
 
+        p_cpm <- evamtools:::probs_from_trm(out$CBN_trans_rate_mat, all_genotypes = TRUE)
+        p_cpm <- evamtools:::reorder_to_pD(p_cpm)
+        
+        ## slows down a lot with larger n
+        p_cpm_sampl <- evamtools:::population_sample_from_trm(out$CBN_trans_rate_mat,
+                                                              n_samples = 1e7)
+        
+        v_sampl <- evamtools:::sample_to_pD_order(p_cpm_sampl$obs_events,
+                                                  ngenes = ncol(out$analyzed_data),
+                                                  gene_names = colnames(out$analyzed_data))
+        
+        names(v_sampl) <- evamtools:::generate_sorted_genotypes(n_genes = ncol(out$analyzed_data),
+                                                                gene_names = colnames(out$analyzed_data))
+        v_sampl <- v_sampl/sum(v_sampl)
+
+        t(apply(cbind(p_cpm, v_sampl), 1,
+                function(y) {return(c(diff = y[1] - y[2],
+                                      rel_diff = abs(y[1]- y[2])/min(y)))}))
+
+        ## ######################################################################
+        ## HESBCN. Examples where genotype with all loci mutated is not accessible
+
+
+        set.seed(2)
+        d1 <- data.frame(A = sample(c(1, 0), prob = c(0.7, 0.2), size = 200, replace = TRUE),
+                         B = sample(c(1, 0), prob = c(0.85, 0.2), size = 200, replace = TRUE))
+        d1$C <- 0
+        d1$D <- 0
+        d1$E <- 0
+        d1$F <- 0
+        d1$C[(d1$A == 1) & (d1$B == 1)] <- 1
+        d1$D[(d1$A == 1) | (d1$B == 1)] <- 1
+        d1$D[100:200] <- 0
+        d1$E[xor((d1$A == 1), (d1$B == 1))] <- 1
+        d1$F[(d1$C == 1) & (d1$D == 1)] <- 1
+        d2 <- rbind(d1,
+                    data.frame(A = sample(c(1, 0), size = 25, prob = c(0.5, 0.2), replace = TRUE),
+                               B = sample(c(1, 0), size = 25, prob = c(0.5, 0.2), replace = TRUE),
+                               C = 0,
+                               D = sample(c(1, 0), size = 25, replace = TRUE),
+                               E = 0,
+                               F = 0))
+        d3 <- d2
+        d3$C[(d3$A == 1) & (d3$B == 1)] <- 1
+
+        ## Examples that mix output
+        ## d3_2 <- evamtools:::do_HESBCN(d3, seed = 31)  ## AND, XOR, Single
+        set.seed(NULL)
+
+        out2 <- evam(d3, methods = c("HESBCN"), hesbcn_opts = list(seed = 31))
+        
+        p2_cpm <- evamtools:::probs_from_trm(out2$HESBCN_trans_rate_mat, all_genotypes = TRUE)
+        p2_cpm <- evamtools:::reorder_to_pD(p2_cpm)
+        
+        ## slows down a lot with larger n
+        p2_cpm_sampl <- evamtools:::population_sample_from_trm(out2$HESBCN_trans_rate_mat,
+                                                               n_samples = 1e6)
+        
+        v2_sampl <- evamtools:::sample_to_pD_order(p2_cpm_sampl$obs_events,
+                                                   ngenes = ncol(out2$analyzed_data),
+                                                   gene_names = colnames(out2$analyzed_data))
+        
+        names(v2_sampl) <- evamtools:::generate_sorted_genotypes(n_genes = ncol(out2$analyzed_data),
+                                                                 gene_names = colnames(out2$analyzed_data))
+
+        chisq.test(x = v2_sampl[p2_cpm > 0], p = p2_cpm[p2_cpm > 0], B = 2000)
+        
+        v2_sampl <- v2_sampl/sum(v2_sampl)
+
+        comps <- t(apply(cbind(p2_cpm, v2_sampl), 1,
+                         function(y) {return(c(diff = y[1] - y[2],
+                                               rel_diff = abs(y[1]- y[2])/min(y)))}))
+        ## So largest relative difference is 3% (of the smallest of the two)
+        ## And largest abs. diff. is 6e-4 so 600 observations
+        summary(comps0)
+
+        ## Run another, much smaller
+        p2b_cpm_sampl <- evamtools:::population_sample_from_trm(out2$HESBCN_trans_rate_mat,
+                                                                n_samples = 1e5)
+        
+        v2b_sampl <- evamtools:::sample_to_pD_order(p2b_cpm_sampl$obs_events,
+                                                    ngenes = ncol(out2$analyzed_data),
+                                                    gene_names = colnames(out2$analyzed_data))
+        
+        names(v2b_sampl) <- evamtools:::generate_sorted_genotypes(n_genes = ncol(out2$analyzed_data),
+                                                                  gene_names = colnames(out2$analyzed_data))
+
+
+        chisq.test(x = v2b_sampl[p2_cpm > 0], p = p2_cpm[p2_cpm > 0], B = 2000)
+        
+        
+        v2b_sampl <- v2b_sampl/sum(v2b_sampl)
+
+        comps <- t(apply(cbind(p2_cpm, v2b_sampl), 1,
+                         function(y) {return(c(diff = y[1] - y[2],
+                                               rel_diff = abs(y[1]- y[2])/min(y)))}))
+        summary(comps)
+    })
+}
