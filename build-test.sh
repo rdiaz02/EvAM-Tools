e1395ead2284dfb2aebbecfbfb095396352b4ed7
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-02-16 16:19:53 +0100

    using Rbuildignore

--- build-test.sh
+++ build-test.sh
@@ -1,6 +1,7 @@
 rm -r -f evamtools.Rcheck
 V_PKG=$(cat ./evamtools/DESCRIPTION | grep Version | cut -d' ' -f2)
 rm evamtools_$V_PKG.tar.gz
+rm -r -f ./evamtools/inst/miscell/auto
 R_ENVIRON_USER=./Renviron.based_on_bioc R CMD build evamtools
 R_ENVIRON_USER=./Renviron.based_on_bioc R CMD INSTALL evamtools_$V_PKG.tar.gz
 R_ENVIRON_USER=./Renviron.based_on_bioc R CMD check evamtools_$V_PKG.tar.gz
