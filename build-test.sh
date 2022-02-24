081217dfbe7ad80d6e16950e3a13d347f4477549
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-02-24 11:55:00 +0100

    building scripts and check conditions

--- build-test.sh
+++ build-test.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 rm -r -f evamtools.Rcheck
 V_PKG=$(cat ./evamtools/DESCRIPTION | grep Version | cut -d' ' -f2)
 rm evamtools_$V_PKG.tar.gz
 rm -r -f ./evamtools/inst/miscell/auto
-R_ENVIRON_USER=./Renviron.based_on_bioc R CMD build evamtools
-R_ENVIRON_USER=./Renviron.based_on_bioc R CMD INSTALL evamtools_$V_PKG.tar.gz
-R_ENVIRON_USER=./Renviron.based_on_bioc R CMD check evamtools_$V_PKG.tar.gz
+R_ENVIRON_USER=./Renviron.based_on_bioc R --no-site-file --no-init-file CMD build evamtools
+R_ENVIRON_USER=./Renviron.based_on_bioc R --no-site-file --no-init-file CMD INSTALL --install-tests evamtools_$V_PKG.tar.gz
+R_ENVIRON_USER=./Renviron.based_on_bioc R --no-site-file --no-init-file CMD check evamtools_$V_PKG.tar.gz
 
