6000016fbb2c9ebd01ed21ae4f070cfd76b3909b
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-03-23 22:32:44 +0100

    browser choice, and do not modify user options
    - use firefox, if any (free, in contrast to google chrome)
    - do not alter possible user options for browser (reset on exit)

--- evamtools/R/runShiny.R
+++ evamtools/R/runShiny.R
@@ -14,15 +14,13 @@
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-#' @title Run the web application of evamtools
-#' 
-#' @description  Launch the server with the web based app
 runShiny <- function(host="0.0.0.0", port = 3000) {
   appDir <- system.file("shiny-examples", "evamtools", package = "evamtools")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `evamtools`.", call. = FALSE)
   }
 
-  options(shiny.autoreload = TRUE, browser = "/usr/bin/google-chrome")
+  opt <- options(shiny.autoreload = TRUE, browser = "/usr/bin/firefox")
+  on.exit(options(opt))
   shiny::runApp(appDir, port = port, host = host, display.mode = "normal")
 }
