4c00db7a24938fadfcd2413e5f3738c8acebbcd2
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-03-29 23:18:22 +0200

    ooops, typo

--- Additional_doc/create_all_additional_doc.sh
+++ Additional_doc/create_all_additional_doc.sh
@@ -3,4 +3,4 @@
 texi2pdf intro_additional_docs.tex
 texi2pdf Additional_doc.tex
 texi2pdf ../evamtools/inst/miscell/Using_OncoSimulR_to_get_accessible_genotypes_trans_mats.tex
-pdftk intro_additional_docs.pdf Additional_doc.pdf ../evamtools.Rcheck/evamtools-manual.pdf Using_OncoSimulR_to_get_accessible_genotypes_trans_mats cat output Addtional_doc_all.pdf
+pdftk intro_additional_docs.pdf Additional_doc.pdf ../evamtools.Rcheck/evamtools-manual.pdf Using_OncoSimulR_to_get_accessible_genotypes_trans_mats cat output Additional_doc_all.pdf
