6f8d87e36745bf004b27645a98800bde0b051503
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-04-25 13:15:42 +0200

    clarified why we are using --shell-escape

--- Additional_doc/create_all_additional_doc.sh
+++ Additional_doc/create_all_additional_doc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## We need to use "--shell-escape" for latexgit.
+
 texi2pdf --shell-escape intro_additional_docs.tex
 texi2pdf --shell-escape Additional_doc.tex
 texi2pdf --shell-escape ../evamtools/inst/miscell/Using_OncoSimulR_to_get_accessible_genotypes_trans_mats.tex
