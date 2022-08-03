75e9347eab61a5050c5d5eda4bfc8f46189aec38
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-08-03 22:33:27 +0200

    a very looooong tooltip gene num changes and MHN resample

--- evamtools/inst/shiny-examples/evamtools/server.R
+++ evamtools/inst/shiny-examples/evamtools/server.R
@@ -693,25 +693,60 @@ server <- function(input, output, session, EVAM_MAX_ELAPSED = 1.5 * 60 * 60) {
     ## Define number of genes
     output$gene_number_slider <- renderUI({
         val <- ifelse(is.null(data$n_genes), 3, data$n_genes)
-                                        # tag$div(class="frame",
-        if (input$input2build %in% c("csd", "dag", "matrix")){
 
+        if (input$input2build %in% c("csd","dag", "matrix")) {
             tags$div(class = "frame flex",
                      tags$h3("Set the number of genes"),
                      tags$h5("(Using 7 or more genes can lead ",
                              "to very long execution times for some methods ",
                              "and crowded figures.)"),
-                                        
+                     
                      tags$div(class="inlin",
                               tags$h3(HTML("<br/>")),
                               sliderInput("gene_number", "Number of genes",
                                           value = val, max = max_genes, min = min_genes,
-                                          step = 1)
+                                          step = 1),
                               ),
                      tags$h4(HTML("<br/>")),
                      actionButton("change_gene_names", "Change gene names"),
                      )
-        }
+        } ## too much for a tooltip
+        ## else if (input$input2build %in% c("dag", "matrix")) {
+        ##   tags$div(class = "frame flex",
+        ##            tags$h3("Set the number of genes"),
+        ##            tags$h5("(Using 7 or more genes can lead ",
+        ##                    "to very long execution times for some methods ",
+        ##                    "and crowded figures.)"),
+        
+        ##            tags$div(class="inlin",
+        ##                     tags$h3(HTML("<br/>")),
+        ##                     sliderInput("gene_number", "Number of genes",
+        ##                                 value = val, max = max_genes, min = min_genes,
+        ##                                 step = 1),
+        ##                     shinyBS::bsTooltip("gene_number",
+        ##                                        paste("With MHN (but not DAG) ",
+        ##                                              "changing number of genes forces ",
+        ##                                              "the generation of data from the model ",
+        ##                                              "to prevent inconsistent states between ",
+        ##                                              "the data and the model. This is on purpose. ",
+        ##                                              "(Under MHN, adding a gene amounts to adding a row ",
+        ##                                              "and a column, and removing a gene ",
+        ##                                              "removing a row and a column; if we ",
+        ##                                              "did not force a resample and you ",
+        ##                                              "forgot to do it, the genotype data ",
+        ##                                              "could be left in a state completely ",
+        ##                                              "inconsistent with the model. ",
+        ##                                              "In contrast, with DAGs, changing ",
+        ##                                              "gene number has no effect on the ",
+        ##                                              "model until you add/remove edges.)"),
+        ##                                        "right"
+        ##                                      , options = list(container = "body")),
+        ##                     ),
+        ##            tags$h4(HTML("<br/>")),
+        ##            actionButton("change_gene_names", "Change gene names"),
+        ##            )
+        ##   }
+        
     })
 
     observeEvent(input$change_gene_names, {
@@ -1494,7 +1529,9 @@ server <- function(input, output, session, EVAM_MAX_ELAPSED = 1.5 * 60 * 60) {
                 rownames(data$csd_counts) <- data$csd_counts$Genotype
                 data$csd_counts[, 2] <- as.numeric(data$csd_counts[, 2])
                 ## Filtering out non-positive counts
-                data$csd_counts <- data$csd_counts[data$csd_counts[, 2] > 0,]
+                data$csd_counts <-
+                    evamtools:::reorder_to_standard_order_count_df(
+                                    data$csd_counts[data$csd_counts[, 2] > 0, ])
                 data$data <-
                     datasets$all_csd[[input$input2build]][[input$select_csd]]$data <-
                         evamtools:::genotypeCounts_to_data(data$csd_counts, e = 0)
