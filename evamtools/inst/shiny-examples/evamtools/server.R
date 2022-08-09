0a82ecdee5af22ce9c0af8a2f6b42a494bd37b25
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-08-09 12:35:25 +0200

    allow some HTML mark up from stop to show in modals

--- evamtools/inst/shiny-examples/evamtools/server.R
+++ evamtools/inst/shiny-examples/evamtools/server.R
@@ -21,10 +21,12 @@ dataModal <- function(error_message, type = "Error: ") {
     if (type == "Error: ") {
         type <- HTML("<font color ='red'>", type, "</font color>")
     }
+
+    error_message_htmlized <- gsub("\n", "<br>", error_message)
     modalDialog(
         easyClose = TRUE,
         title = tags$h3(type),
-        tags$div(HTML(error_message))
+        tags$div(HTML(error_message_htmlized))
     )
 }
 
