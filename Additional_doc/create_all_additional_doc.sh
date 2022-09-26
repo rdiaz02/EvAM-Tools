960a974bf98fd350fd788e157fb6841fb2615e2c
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-09-27 00:26:34 +0200

    renamed files and title

--- Additional_doc/create_all_additional_doc.sh
+++ Additional_doc/create_all_additional_doc.sh
@@ -9,14 +9,14 @@ texi2pdf --shell-escape Additional_tech_doc.tex
 
 texi2pdf --shell-escape ../evamtools/inst/miscell/Using_OncoSimulR_to_get_accessible_genotypes_trans_mats.tex
 
-./runKnitr-2.sh  evamtools-examples.Rnw
+./runKnitr-2.sh  evamtools_examples.Rnw
 
 pdftk intro_additional_docs.pdf Additional_tech_doc.pdf evamtools-examples.pdf ../evamtools.Rcheck/evamtools-manual.pdf Using_OncoSimulR_to_get_accessible_genotypes_trans_mats.pdf cat output Additional_doc_all.pdf
 
 cp Additional_doc_all.pdf ../../evamtools-gh-pages/pdfs/.
 
 cp Additional_tech_doc.pdf ../../evamtools-gh-pages/pdfs/.
-cp evamtools-examples.pdf ../../evamtools-gh-pages/pdfs/.
+cp evamtools_examples.pdf ../../evamtools-gh-pages/pdfs/.
 cp ../evamtools.Rcheck/evamtools-manual.pdf ../../evamtools-gh-pages/pdfs/.
 cp Using_OncoSimulR_to_get_accessible_genotypes_trans_mats.pdf ../../evamtools-gh-pages/pdfs/.
 
