6f3b4b8552abc1c2a260a5f60757b90d2bbdfa49
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2022-10-01 16:40:49 +0200

    script doc creating fixes

--- Additional_doc/create_all_additional_doc.sh
+++ Additional_doc/create_all_additional_doc.sh
@@ -14,7 +14,7 @@ Rscript --vanilla -e 'library(knitr); knit("evamtools_examples.Rnw")'
 texi2pdf --shell-escape evamtools_examples.tex
 
 
-pdftk intro_additional_docs.pdf evamtools_methods_details_faq.pdf evamtools-examples.pdf ../evamtools.Rcheck/evamtools-manual.pdf Using_OncoSimulR_to_get_accessible_genotypes_trans_mats.pdf cat output Additional_doc_all.pdf
+pdftk intro_additional_docs.pdf evamtools_methods_details_faq.pdf evamtools_examples.pdf ../evamtools.Rcheck/evamtools-manual.pdf Using_OncoSimulR_to_get_accessible_genotypes_trans_mats.pdf cat output Additional_doc_all.pdf
 
 cp Additional_doc_all.pdf ../../evamtools-gh-pages/pdfs/.
 
