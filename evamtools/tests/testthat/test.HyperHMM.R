0e0ed968dc2505dd42a26e95311579c8d3113c08
Erios12 <eriosjimenez00@gmail.com>
2023-01-07 02:05:13 +0100

    New test file for HyperHMM to be completed

---
+++ evamtools/tests/testthat/test.HyperHMM.R
@@ -0,0 +1,11 @@
+## Testing HyperHMM
+
+t1 <- Sys.time()
+test_that("Testing HyperHMM integration in EvAM-Tools", {
+
+    ## For testing
+    
+})
+
+cat("\n Done test.HyperHMM.R. Seconds = ",
+    as.vector(difftime(Sys.time(), t1, units = "secs")), "\n")
