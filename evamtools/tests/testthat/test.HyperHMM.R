a7ffc0a7ecf624690af22a30f502cc6ba5da8748
pavillos <pablo.villoslada@gmail.com>
2023-01-08 19:21:06 +0100

    test added

--- evamtools/tests/testthat/test.HyperHMM.R
+++ evamtools/tests/testthat/test.HyperHMM.R
@@ -2,9 +2,11 @@
 
 t1 <- Sys.time()
 test_that("Testing HyperHMM integration in EvAM-Tools", {
-
-    ## For testing
-    
+    data(every_which_way_data)
+    Dat1 <- every_which_way_data[[16]][1:40, 2:6]
+    out <- suppressMessages(evam(Dat1, methods = 'HyperHMM'))
+    expect_true(inherits(out$HyperHMM_trans_mat, 'Matrix'))
+    expect_true(all(slotNames(out$HyperHMM_trans_mat)==c('i','p','Dim','Dimnames','x','factors')))        
 })
 
 cat("\n Done test.HyperHMM.R. Seconds = ",
