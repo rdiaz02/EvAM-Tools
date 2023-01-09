8db6addf6872b2dd02d1a5bfcf1c098b8e2d20ab
pavillos <pablo.villoslada@gmail.com>
2023-01-09 19:34:13 +0100

    final test added

--- evamtools/tests/testthat/test.HyperHMM.R
+++ evamtools/tests/testthat/test.HyperHMM.R
@@ -8,10 +8,11 @@ test_that("Testing HyperHMM integration in EvAM-Tools", {
     out <- suppressMessages(evam(data, methods = 'HyperHMM'))
     expect_true(inherits(out$HyperHMM_trans_mat, 'Matrix'))
     expect_true(all(slotNames(out$HyperHMM_trans_mat)==c('i','p','Dim','Dimnames','x','factors')))
-    #lista<-lapply(1:length(out$HyperHMM_features),
-    #              function(y) combn(out$HyperHMM_features,y,simplify=FALSE))
-    #lista<-c('WT',unlist(lapply(lista, 
-    #                            function(y) (lapply(y, function(z) toString(z))))))
+    lista<-lapply(1:length(out$HyperHMM_features),
+                  function(y) combn(out$HyperHMM_features,y,simplify=FALSE))
+    lista<-c('WT',unlist(lapply(lista, 
+                                function(y) (lapply(y, function(z) toString(z))))))
+    expect_true(all(out$HyperHMM_trans_mat@Dim==c(length(lista), length(lista))))
 })
 
 cat("\n Done test.HyperHMM.R. Seconds = ",
