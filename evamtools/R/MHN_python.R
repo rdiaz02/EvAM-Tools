4e83425a77df7d2706440c3016001a7a072c631f
Ramon @ Triturus <no_specific_email>
2025-04-10 13:47:07 +0200

    almost done

--- evamtools/R/MHN_python.R
+++ evamtools/R/MHN_python.R
@@ -4,16 +4,12 @@
 ## But since this might not yet be fully integrated into evam
 ## I may add a few things here that will later be moved out
 
-## FIXME: pass the path to python
-## e.g., ~/.local/python3.12-venv/bin/
-
 
 library(evamtools)
 library("reticulate")
 
-
-
-## This is somewhat time consuming, so do it once per session?
+## This is somewhat time consuming, so do it once per session
+## Pass the venv you use, most likely 3.12
 setup_python_MHN <- function(python_version = "~/.local/python3.12-venv/bin/python3.12") {
   use_python(python = python_version)
   mhn <- import("mhn")
@@ -52,12 +48,27 @@ set_run_cv_MHN_python <- function(opts) {
                        ")"))
 }
 
-
+## matrix, list -> output
+##  opts is the opts$MHN_python list
 run_MHN_python <- function(x, opts) {
+  check_MHN_python_options(opts)
   RhpcBLASctl::omp_set_num_threads(opts$omp_threads)
   time_out <- system.time({
-    out <- do_MHN_python(x, FIXME_lambda = opts$lambda)
+    outp <- do_MHN_python(x, opts)
+    out <- list()
+    theta <- outp$log_theta
+    colnames(theta) <- rownames(theta) <- colnames(x)
+    out$theta <- theta
+    rm(theta)
+    out$transitionRateMatrix <- theta_to_trans_rate_3(out$theta)
     out <- c(out,
+             lambda_used = outp$meta$lambda,
+             maxit       = outp$meta$maxit,
+             reltol       = outp$meta$reltol,
+             score = outp$meta$score,
+             message = outp$meta$message,
+             status = outp$meta$status,
+             nit = outp$meta$nit,
              predicted_genotype_freqs =
                list(probs_from_trm(out$transitionRateMatrix))
              )
@@ -89,18 +100,48 @@ opt.load_data_matrix(data_matrix)
 }
 
 
-
+## matrix, list (this is the MHN_python list component)
 do_MHN_python <- function(x,
-                          opts
-                          ## dirname = NULL,
-                          ## filename = "mhn_input.csv",
-                          ## silent = TRUE,
-                          ## rmfile = TRUE
-                          ) {
-  set_MHN_python_options(opts$MHN_python)
+                          opts) {
+  if (opts$verbose) message("Starting.")
+  if (opts$verbose) message("     setting options")
+  set_MHN_python_options(opts)
+  if (opts$verbose) message("     loading data")
   data_2_MHN_load_data_matrix(x)
-  set_run_cv_MHN_python(opts$MHN_python)
-  train_MHN_python(opts$MHN_python)
-  MHN_result <- py$opt$result
-
+  if (opts$verbose) message("     starting run_cv")
+  set_run_cv_MHN_python(opts)
+  if (opts$verbose) message("     starting training")
+  train_MHN_python(opts)
+  if (opts$verbose) message("Finished run.")
+  ## MHN_result <- py$opt$result
+  return(py$opt$result)
 }
+
+
+check_MHN_python_options <- function(opts) {
+  stopifnot(is.numeric(opts$omp_threads))
+  stopifnot(opts$Type %in% c("oMHN", "cMHN"))
+  stopifnot(opts$Penalty %in% c("L1", "L2", "SYM_SPARSE"))
+  stopifnot(is.numeric(opts$lambda_min))
+  stopifnot(is.numeric(opts$lambda_max))
+  stopifnot(is.numeric(opts$steps))
+  stopifnot(is.numeric(opts$nfolds))
+  stopifnot(is.numeric(opts$maxit))
+  stopifnot(is.logical(opts$verbose))
+}
+
+
+## The next two make code possibly harder to understand
+## to set lambda_min and lambda_max from the object itself
+eval_MHN_python_opts <- function(opts, x){
+  return(c(opts, lambda_min = 0.1/nrow(x), lambda_max = 100/nrow(x)))
+}
+
+## wrapper of a single argument
+wrap_run_MHN_python <- function(x, opts) {
+  run_MHN_python(ddd, opts = eval_MHN_python_opts(opts, x))
+}
+
+
+library(codetools)
+checkUsageEnv(env = .GlobalEnv)
--- evamtools/R/mhn_python_example.R
+++ evamtools/R/mhn_python_example.R
@@ -9,20 +9,30 @@ setup_python_MHN()
 
 
 
-opts <- list()
-opts$MHN_python <- list(
+opts0_MHN_python <- list(
+  omp_threads = 1, ##
   Type = "cMHN", ## "oMHN" won't be used for now
   Penalty = "SYM_SPARSE", ## or L1 or L2. We'll use SYM and L1
   seed = NA,
-  ## FIXME: use the usual apparatus
-  lambda_min = 0.001, ## 0.1/nrow(x),
-  lambda_max = 0.1, ## 100/nrow(x),
   steps = 4, # 10,
   nfolds = 3, # 5,
   ## Default is 5000
   maxit = 100, ## 5000
-  show_progressbar = "True"
+  show_progressbar = "False",
+  verbose = TRUE
 )
 
-## Each run
-do_MHN_python(ddd, opts)
+## ## Each run
+## do_MHN_python(ddd, opts)
+
+
+
+
+r1 <- run_MHN_python(ddd, opts = c(opts0_MHN_python,
+                                   lambda_min = 0.1/nrow(ddd),
+                                   lambda_max = 100/nrow(ddd)))
+
+## FIXME
+## return the lambda used
+## return the maxit, message, nit, etc
+## compare the predicted genotypes
