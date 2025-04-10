56be38962a34efae7bec51879f1573c0f68b3aa6
Ramon Diaz-Uriarte <rdiaz02@gmail.com>
2025-04-10 09:58:12 +0200

    more MHN python

--- evamtools/R/MHN_python.R
+++ evamtools/R/MHN_python.R
@@ -4,7 +4,46 @@
 ## But since this might not yet be fully integrated into evam
 ## I may add a few things here that will later be moved out
 
+## FIXME: pass the path to python
+## e.g., ~/.local/python3.12-venv/bin/
+
+library("reticulate")
+
+opts$MHN_python <- list(
+  ##   python = "~/.local/python3.12-venv/bin/python3.12",
+  Type = "cMHN",
+  Penalty = "SYM_SPARSE",
+  seed = NA,
+  ## FIXME: use the usual apparatus
+  lambda_min = 0.001, ## 0.1/nrow(x),
+  lambda_max = 0.1, ## 100/nrow(x),
+  steps = 4, # 10,
+  nfolds = 3, # 5,
+  ## Default is 5000
+  maxit = 100, ## 5000
+  show_progressbar = "True"
+)
+
+## This is somewhat time consuming, so do it once per session?
+setup_python_MHN <- function(python_version = "~/.local/python3.12-venv/bin/python3.12") {
+  use_python(python = python_version)
+  mhn <- import("mhn")
+  mhn_version <- py_get_attr(mhn, "__version__")
+  message("Using MHN version ", mhn_version,
+          " with Python ", python_version)
+  py_run_string("from mhn.optimizers import Optimizer")
+  ## For data handling
+  py_run_string("
+import numpy as np
+import pandas as pd
+")
+
+}
+
+
+
 run_MHN_python <- function(x, opts) {
+
   RhpcBLASctl::omp_set_num_threads(opts$omp_threads)
   time_out <- system.time({
     out <- do_MHN_python(x, FIXME_lambda = opts$lambda)
@@ -18,51 +57,76 @@ run_MHN_python <- function(x, opts) {
 }
 
 
+## Might change during session
+set_MHN_python_options <- function(opts) {
+  ## FIXME: we need error checking for options
+  ## Type: one of cMHN or oMHN
+  py_run_string(paste0("opt = Optimizer(Optimizer.MHNType.", opts$Type, ")"))
+  ## Penalty: one of L1, L2, SYM_SPARSE
+  py_run_string(paste0("opt.set_penalty(opt.Penalty.", opts$Penalty, ")"))
+  ## Leave init_theta at default. So do not touch it
+}
+
+## After reading the data
+set_MHN_python_options_2 <- function(opts) {
+  if(!is.null(opts$seed) && !is.na(opts$seed)) {
+    py_run_string(paste0("mhn.set_seed(", opts$seed, ")"))
+  }
+  py_run_string(paste0("cv_lambda = opt.lambda_from_cv(",
+                       "lambda_min=", opts$lambda_min, ",",
+                       "lambda_max=", opts$lambda_max, ",",
+                       "steps=", opts$steps, ",",
+                       "nfolds=", opts$nfolds, ",",
+                       "show_progressbar=", opts$show_progressbar,
+                       ")"))
+}
+
+train_MHN_python <- function(opts) {
+  py_run_string(paste0("opt.train(",
+                       "lam=cv_lambda,",
+                       "maxit=", opts$maxit, ",",
+                       "round_result=False,",
+                       ")"
+                       ))
+
+}
 
 
 
 do_MHN_python <- function(x,
                           dirname = NULL,
-                          addname = NULL,
+                          filename = "mhn_input.csv",
                           silent = TRUE,
                           rmfile = TRUE) {
-  ## Setup the tmp directory structure, like for cbn
-  ## Check the python executable is there
 
+  set_MHN_python_options(opts$MHN_python)
 
+  data_2_MHN_load_data_matrix(x)
 
-  if(is.null(dirname)) {
-    dirname <- tempfile()
-    dirname0 <- NULL
-    if(!is.null(addname)) {
-      dirname0 <- dirname
-      dirname <- paste0(dirname, "/",
-                        "_cbn_", init.poset, "_",
-                        addname)
-    }
-    if(!silent)
-      message(paste("\n Using dir", dirname))
-    if(dir.exists(dirname)) {
-      stop("dirname ", dirname, "exists")
-    }
-    dir.create(dirname, recursive = TRUE)
-  }
+  set_MHN_python_options_2(opts$MHN_python)
+  train_MHN_python(opts$MHN_python)
+
+  MHN_result <- py$opt$result
+
+}
 
 
 
 
 
-  ## FIXME: adapt all this to MHN
-  if(rmfile) {
-    files.created <- paste(dirname, c(".pat", ".prf", ".log", ".poset",
-                                      ".lambda"), sep = "")
-    file.remove(files.created)
-    try(file.remove(paste0(dirname, ".lambda_i"))) ## for the hack that produces lambda_i
-    try(file.remove(paste0(dirname, ".logliknew"))) ## for the 2nd hack that produces logliknew
-    ## to get the lambda, iterative, file
-    file.remove(paste(dirname, "/00000.poset", sep = ""))
-    file.remove(dirname)
-    if(!is.null(dirname0))
-      file.remove(dirname0)
-  }
+
+rc <- random_evam(model = "CBN", ngenes = 5)
+rd <- sample_evam(rc, N = 1000, obs_noise = 0.05)
+ddd <- rd[[2]]
+
+
+
+data_2_MHN_load_data_matrix <- function(x) {
+  py <- reticulate::py
+  py$x <- x
+  py$colnames_x <- colnames(x)
+  py_run_string("
+data_matrix = pd.DataFrame(np.array(x, dtype=np.int32), columns=colnames_x)
+opt.load_data_matrix(data_matrix)
+  ")
 }
